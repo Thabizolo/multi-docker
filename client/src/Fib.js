b28827c568623a72a8a0e4a8c1d5844aec38ddc7
Phumelele Msimang <phumelele.msimang@wesbank.co.za>
2019-11-20 11:24:07 +0200

    changed Fib.js file and commented out depl script on travis

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,6 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
+    this.state = { seenIndexes: [] };
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
